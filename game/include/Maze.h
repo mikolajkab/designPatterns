eb35feea353df0ce9cf68581036784c275c30438
Mikolaj Kabacinski <mikolaj.kabacinski@gmail.com>
2018-07-25 20:58:22 +0200

    created MazeBuilder

---
+++ game/include/Maze.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Maze
+{
+  public:
+    Maze();
+    virtual ~Maze();
+};
---
+++ game/include/MazeBuilder.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Maze.h"
+
+class MazeBuilder
+{
+  public:
+    virtual void BuildMaze() = 0;
+    virtual void BuildRoom() = 0;
+    virtual void BuildDoor(int roomFrom, int roomTo) = 0;
+
+    virtual Maze *GetMaze() = 0;
+};
